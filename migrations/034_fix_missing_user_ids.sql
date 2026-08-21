6d51cfd21e020a27a533dd1607e85d658521d4df
Richco <you@example.com>
2026-08-21 16:04:20 -0400

    Update migration 034: Add explicit Joanna user_id fix
    - Added specific update for joannat@richcogroup.com user_id mapping
    - Joanna is now properly linked to her auth user account

--- migrations/034_fix_missing_user_ids.sql
+++ migrations/034_fix_missing_user_ids.sql
@@ -6,6 +6,7 @@ SET user_id = au.id
 FROM auth.users au
 WHERE cm.email = au.email AND cm.user_id IS NULL;
 
--- Log the update count
--- The UPDATE statement above will populate user_id for all crew members
--- that have matching emails in auth.users but currently have NULL user_id
+-- Also handle specific case: Joanna T at joannat@richcogroup.com
+UPDATE public.crew_members
+SET user_id = 'c73cbb0b-9524-4e6a-a2aa-680fa7d997db'
+WHERE email = 'joannat@richcogroup.com' AND user_id IS NULL;
